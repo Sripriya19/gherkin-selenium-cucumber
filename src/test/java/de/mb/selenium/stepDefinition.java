6c671f6459f65e10968d240599f3c2932cfbee8c
Sripriya19 <34740767+Sripriya19@users.noreply.github.com>
2017-12-21 17:47:50 +0530

    Update stepDefinition.java

--- src/test/java/de/mb/selenium/stepDefinition.java
+++ src/test/java/de/mb/selenium/stepDefinition.java
@@ -25,14 +25,14 @@ public class stepDefinition {
 	@Given("^The selenium setup is complete$")
 	public void setUp() throws MalformedURLException {
 		String serverUrl = System.getProperty("grid.server.url");
-		String gridServerUrl = "http://10.144.2.237:4444/wd/hub";
+		String gridServerUrl = "http://seleniumhub:4444/wd/hub";
 		if (serverUrl != null) {
 			gridServerUrl = serverUrl;
 		}
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		URL gridUrl = new URL(gridServerUrl);
 		driver = new RemoteWebDriver(gridUrl, capability);
-		driver.get("http://10.144.2.14:80");
+		driver.get("http://tomcat:10000");
 	}
 
 	@When("^print a simple message$")
